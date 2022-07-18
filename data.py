5d1854492054e5e1f01401886e827d2fa319c001
Jack Newsom <jack@moveparallel.com>
2022-07-18 10:35:46 -0700

    rename pull luminar and update gitignore

--- data.py
+++ data.py
@@ -1,3 +1,8 @@
+"""Shim for getting TXYZ files into LOAM
+"""
+
+
+import cv2
 import json
 import os
 import shutil
@@ -47,3 +52,55 @@ def pull_files(idxs: list):
             LIDAR_DIR + "xyz_new/" + meta[i][1],
             DATA_DIR
         )
+
+def timestamp_from_txyz(file_name):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+    time = lines[0].split(",")[0]
+    return float(time)
+
+def pull_video(meta):
+    file_name, num_frames, mapping = meta["file_name"], meta["num_frames"], meta["map"]
+
+    cap = cv2.VideoCapture(file_name)
+    c = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        cv2.imwrite(f"{DATA_DIR}{c}.png", frame)
+        c += 1
+
+    if c != num_frames:
+        raise ValueError(f"meta says num_frames = {num_frames}, only {c+1} found")
+    
+    names = [m[1] for m in mapping]
+    times = [timestamp_from_txyz(LIDAR_DIR + "xyz_new/" + n) for n in names]
+
+    with open(f"{DATA_DIR}times.txt", "w") as f:
+        f.writelines([f'{t}\n' for t in times])
+
+    with open(f"{DATA_DIR}files.txt", "w") as f:
+        f.writelines([f'{n}\n' for n in names])
+
+    for n in names:
+        shutil.copy(
+            LIDAR_DIR + "xyz_new/" + n,
+            DATA_DIR
+        )
+
+
+if __name__ == "__main__":
+    import random
+    N_SCENES = 10
+    SCENE_LENGTH = 500
+
+    selected = []
+    valid_scenes = range(len(meta) // SCENE_LENGTH)
+    while len(selected) < N_SCENES:
+        scene = random.choice(valid_scenes)
+        scene = range(scene)
+        try:
+            pull_files()
+
